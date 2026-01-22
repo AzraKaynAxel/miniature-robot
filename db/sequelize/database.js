4bcba765563225de22b28dea6d18349d9cc9a2fa
AzraKaynAxel <axforjob.hay@gmail.com>
2026-01-22 09:49:15 +0100

    Code standar pour la DB

---
+++ db/sequelize/database.js
@@ -0,0 +1,24 @@
+// Exo SQlite
+const { Sequelize } = require('sequelize');
+
+const myDB = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+    logging: false
+});
+
+const connectDB = async () => {
+    try {
+        await myDB.authenticate();
+        console.log('Connection to SQLite has been established successfuly.');
+        
+        // Viens vérifier les models existant et les mets à jour
+        myDB.sync({alter: true});
+
+        console.log('All models were synchronized succesfully');
+    } catch (error) {
+        console.error(('Unable to connect to databse: ' + error));             
+    }
+};
+
+module.exports = {myDB, connectDB};
