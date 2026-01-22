f07e6e48cef787651469a718597ff516ae3f2b86
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-01-22 14:52:36 +0100

    Feature/controllers (#7)
    * Code standar pour la DB
    
    * Ajout du ReadMe
    
    * MAJ ReadMe
    
    * Création des routes (User,Course,Category
    
    * MAJ ReadMe
    
    * Création des controllers

---
+++ controllers/authController.js
@@ -0,0 +1,42 @@
+const authService = require('../services/authService');
+
+const SingIn = async (req, res) => {
+    try {
+        const myUser = await authService.registerUser(req.body);
+        
+        res.status(201).json({
+            message: 'User registered in successfully',
+            user: {
+                username: myUser.username,
+                email: myUser.email
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });        
+    }
+};
+
+const SingUp = async (req, res) => {
+    try {
+        // Va nous permettre de récupérer des éléments particulier avec la destructuration
+        const { email, password } = req.body;
+
+        // Renvoie le token associé à user
+        const { user, token } = await authService.loginUser(email, password);
+        
+        res.status(200).json({
+            message: 'User logged in successfully',
+            token,
+            user: {
+                id: user.id,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+    console.log("Ma route renvoie bien sur ce controller: login");
+};
+
+module.exports = { SingIn, SingUp };
---
+++ controllers/courseController.js
@@ -0,0 +1,86 @@
+const coursesService = require('../services/coursesService');
+
+const controller = {
+    getAllCourses: async (req, res) => {
+        try {
+            const allCourse = await coursesService.fetchAll();
+            
+            if (!allCourse) {
+                res.status(404).json({message: 'No courses found'}); 
+            }
+
+            res.status(200).json({allCourse});
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
+    },
+    getCourseById: async (req, res) => {
+        try {   
+            const myCourse = await coursesService.fetchById(req.params.id);
+            
+            if (!myCourse) {
+                res.status(404).json({message: 'Course with ' + req.params.id + ' not found'}); 
+            }
+
+            res.status(200).json({myCourse});
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
+    },
+    getCoursesByLevel: async (req, res) => {
+        try {
+            const coursesByLevel = await coursesService.fetchByLevel(req.params.level);
+
+            if (!coursesByLevel) {
+                res.status(404).json({message: 'No courses found for level ' + req.params.level});
+            }
+
+            res.status(200).json({coursesByLevel});
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
+        console.log("Je suis dans getCoursesByLevel");
+    },
+    createCourse: async (req, res) => {
+        try {
+
+            if (!req.body.title) {
+                res.status(400).json({message: 'Please enter valid data for course creation'});
+            }
+
+            const newCourse = await coursesService.createCourse(req.body);
+
+            res.status(201).json({newCourse});
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
+    },
+    updateCourse: async (req, res) => {
+        try {
+            const myUpdateCourse = await coursesService.updateCourse(req.params.id, req.body);
+
+            if (!myUpdateCourse) {
+                res.status(400).json({message: 'Not found course to update'});
+            }
+
+            res.status(201).json({myUpdateCourse});
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
+    },
+    deleteCourse: async (req, res) => {
+        try {
+            const deletedCourse = await coursesService.deleteCourse(req.params.id);
+            
+            if (!deletedCourse) {
+                res.status(400).json({message: 'not found course to delete'});
+            }
+
+            res.status(201).json({deletedCourse});
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
+    }
+};
+
+module.exports = controller;
