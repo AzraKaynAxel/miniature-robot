d6782344c7dea269a83488b54b8413afb8e2f3b4
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-01-23 09:58:42 +0100

    Feature/middleware (#12)
    * Code standar pour la DB
    
    * Ajout du ReadMe
    
    * MAJ ReadMe
    
    * Création des routes (User,Course,Category
    
    * MAJ ReadMe
    
    * Création des controllers
    
    * MAJ ReadMe
    
    * Création des Service (auth, categories, courses)
    
    * Corrective controllers et models
    
    * MAJ ReadMe
    
    * Création du middleware, restriction

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // READ 
 router.get('/', courseController.getAllCourses);
@@ -8,12 +9,12 @@ router.get('/:id', courseController.getCourseById);
 router.get('/level/:level', courseController.getCoursesByLevel);
 
 // CREATE
-router.post('/', courseController.createCourse);
+router.post('/', authMiddleware, courseController.createCourse);
 
 // UPDATE
-router.put('/:id', courseController.updateCourse);
+router.put('/:id', authMiddleware, courseController.updateCourse);
 
 // DELETE
-router.delete('/:id', courseController.deleteCourse);
+router.delete('/:id',authMiddleware, courseController.deleteCourse);
 
 module.exports = router;
